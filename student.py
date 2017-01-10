fe2760a3a886b2fa0438b23572b1f32fa8f6a757
BiroMarton <rezoner9@gmail.com>
2017-01-10 13:19:50 +0100

    implemented person.py and student.py

--- student.py
+++ student.py
@@ -1,5 +1,21 @@
 from person import Person
+import csv
 
 
 class Student(Person):
-    pass
+    Try:
+        def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level):
+            super().__init__(self, first_name, last_name, year_of_birth, gender)
+            self.knowledge_level = knowledge_level
+            self.energy_level = energy_level
+
+        @staticmethod
+        def create_by_csv(path):
+            students = []
+            with open(path, newline="") as data:
+                for line in data:
+                    students.append(line.strip().split(","))
+            return students
+
+    except:
+        raise ValueError
