1a462f7d2ae39d879707c912ce25c67b9b3f3184
Trixi <bozso.beatrix@gmail.com>
2017-01-10 13:39:17 +0100

    mentor.py classmethod done

--- mentor.py
+++ mentor.py
@@ -1,5 +1,20 @@
+import csv
 from person import Person
 
 
 class Mentor(Person):
-    pass
+
+    def __init__(self, nickname):
+        super().__init__(first_name, last_name, year_of_birth, gender)
+        self.nickname = nickname
+
+    @classmethod
+    def create_by_csv(cls, path):
+        data_from_csv = []
+        mentors = []
+
+        with open(path, newline='') as myfile:
+            for line in myfile:
+                data_from_csv.append(line.strip().split(','))
+
+        return data_from_csv
