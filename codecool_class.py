f3904ce9d2c93e288a7f89e9fa2384056702fd4e
Trixi <bozso.beatrix@gmail.com>
2017-01-11 11:13:57 +0100

    classmethod generate_local done

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,20 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+
+    def __init__(self, location, year, mentors, students):
+        self.location = location
+        self.year = year
+        self.mentors = mentors
+        self.students = students
+
+    @classmethod
+    def generate_local(cls):
+        print("Mentors are initialized from CSV")
+        print("Students are initialized from CSV")
+        print("School @ " + location + ", in year " + year + " is created, with " +
+              len(mentors) + " mentors and " + len(students) + " students.")
+
+        return CodecoolClass(location, year, mentors, students)
+
+    def find_student_by_full_name(full_name):
