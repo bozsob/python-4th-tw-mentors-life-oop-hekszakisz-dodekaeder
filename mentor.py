e656fa9bbaf12a9193e97ae5b0d8e5a13b6f0caa
Trixi <bozso.beatrix@gmail.com>
2017-01-11 11:14:51 +0100

    create_by_csv method return value modified

--- mentor.py
+++ mentor.py
@@ -10,11 +10,10 @@ class Mentor(Person):
 
     @classmethod
     def create_by_csv(cls, path):
-        data_from_csv = []
         mentors = []
 
         with open(path, newline='') as myfile:
             for line in myfile:
-                data_from_csv.append(line.strip().split(','))
+                mentors.append(line.strip().split(','))
 
-        return data_from_csv
+        return mentors
