b9ecb6334a3302fbab3d7a887afd869057547b44
Trixi <bozso.beatrix@gmail.com>
2017-01-12 12:01:32 +0100

    find_student_by_full_name method written

--- codecool_class.py
+++ codecool_class.py
@@ -1,22 +1,35 @@
 from mentor import Mentor
 from student import Student
+from person import Person
 
 
 class CodecoolClass:
 
+    location = "Miskolc"
+    year = 2016
+    mentors = Mentor.create_by_csv()
+    students = Student.create_by_csv()
+
     def __init__(self, location, year, mentors, students):
-        self.location = location
-        self.year = year
+        self.location = str(location)
+        self.year = int(year)
         self.mentors = mentors
         self.students = students
 
     @classmethod
     def generate_local(cls):
-        print("Mentors are initialized from CSV")
-        print("Students are initialized from CSV")
-        print("School @ " + location + ", in year " + year + " is created, with " +
-              len(mentors) + " mentors and " + len(students) + " students.")
+        print("Mentors are initialized from CSV.")
+        print("Students are initialized from CSV.")
+        print("School @ {}, in year {} is created, with {} mentors and {} students.".format(
+            cls.location, cls.year, len(cls.mentors), len(cls.students)))
 
-        return CodecoolClass(location, year, mentors, students)
+        return CodecoolClass(cls.location, cls.year, cls.mentors, cls.students)
 
-    def find_student_by_full_name(full_name):
+    def find_student_by_full_name(self, full_name):
+
+        for i in range(len(self.students)):
+            if self.students[i].last_name + " " + self.students[i].first_name == full_name:
+                print("Yes, " + str(self.students[i]) +
+                      " was found between the students.")
+                return self.students[i]
+        print("Not found in students.")
