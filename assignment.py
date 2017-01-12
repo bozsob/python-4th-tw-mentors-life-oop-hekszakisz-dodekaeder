3c9c4b87375893e0e839d8f539f2dec38ca33b73
Trixi <bozso.beatrix@gmail.com>
2017-01-12 14:29:29 +0100

    grading_assignment method modified

--- assignment.py
+++ assignment.py
@@ -5,29 +5,38 @@ import random
 
 class Assignment:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, mentor):
+        self.mentor = mentor
 
     def grading_assignment(self, student):
+        students = Student.create_by_csv()
+        grade = None
 
-        if CodecoolClass.find_student_by_full_name(self, student):
+        for i in range(len(students)):
+            if student == students[i].last_name + " " + students[i].first_name:
+                print("A codecooler írt Assignment-et.")
 
-            grade = (random.randint(1, 12)) * 3
-            print(student + " Assignment-jét értékelték, az eredménye: " +
-                  str(grade) + " pont")
-        if grade > 25:
-            Student.knowledge_level += 5
-            print(student + " tudásszintje egekbe emelkedett, az értéke: " +
-                  Student.knowledge_level)
+                grade = (random.randint(1, 12)) * 3
+                print(
+                    student + " Assignment-jét értékelték, az eredménye: " + str(grade) + " pont")
+                if grade > 25:
+                    students[i].knowledge_level = 100
+                    print(
+                        student + " tudásszintje egekbe emelkedett, az értéke: " + str(students[i].knowledge_level))
+                else:
+                    students[i].knowledge_level = 1
+                    print(
+                        student + " tudásszintje csak sétálgat felfelé. Tanuljon többet!")
+
         return grade
 
-    def making_assignment(self, mentor):
+    def making_assignment(self):
         assignments_list = [
             "Nincs kedvem assignment-et írni, keressetek mást.", "Zöldségek", "OOP - Orbitális Otthoni Palacsinta", "Vegyigyümi"]
         result = random.choice(assignments_list)
-        print("Mentor " + mentor + " a következő Assignment-et írta: " + result)
+        print("Mentor a következő Assignment-et írta: " + result)
 
-oop = Assignment("mentors oop")
+atesz = Assignment("Molnár Attila")
 
-oop.grading_assignment("Trx")
-oop.making_assignment("Atesz")
+atesz.grading_assignment("Bozsó Beatrix")
+atesz.making_assignment()
